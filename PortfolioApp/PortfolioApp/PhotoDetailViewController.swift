6730f69d8ac963e127701abb9a2734e324b54151
spark-01 <>
2024-06-25 16:31:01 +0900

    PhotoDetail画面まで作成

--- PortfolioApp/PortfolioApp/PhotoDetailViewController.swift
+++ PortfolioApp/PortfolioApp/PhotoDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  PhotoInfoViewController.swift
+//  PhotoDetailViewController.swift
 //  PortfolioApp
 //
 //  Created by spark-01 on 2024/06/25.
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class PhotoInfoViewController: UIViewController {
-
+class PhotoDetailViewController: UIViewController {
+    
     @IBOutlet weak var imageView: UIImageView!
     
     var imageURLs: [String] = []
@@ -17,14 +17,8 @@ class PhotoInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadImage(at: currentIndex)
-        
-        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
-        doubleTapGesture.numberOfTapsRequired = 2
-        imageView.addGestureRecognizer(doubleTapGesture)
-        imageView.isUserInteractionEnabled = true
-
     }
-
+    
     func loadImage(at index: Int) {
         let imageURL = imageURLs[index]
         
@@ -48,17 +42,4 @@ class PhotoInfoViewController: UIViewController {
         }.resume()
     }
     
-    @objc func handleDoubleTap(_ gesture: UITapGestureRecognizer) {
-        performSegue(withIdentifier: "ToDetail", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ToDetail" {
-            if let photoDetailVC = segue.destination as? PhotoDetailViewController {
-                photoDetailVC.imageURLs = self.imageURLs
-                photoDetailVC.currentIndex = self.currentIndex
-            }
-        }
-    }
-
 }
