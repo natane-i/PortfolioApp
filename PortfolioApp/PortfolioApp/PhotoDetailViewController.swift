a682dd8e26f072f192cb7521a10152eef7554374
spark-01 <>
2024-06-25 16:41:59 +0900

    ジェスチャー追加 挙動がおかしい

--- PortfolioApp/PortfolioApp/PhotoDetailViewController.swift
+++ PortfolioApp/PortfolioApp/PhotoDetailViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 class PhotoDetailViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var imageURLs: [String] = []
     var currentIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         loadImage(at: currentIndex)
+        
+        setupScrollView()
+        setupTapGestureRecognizer()
     }
     
     func loadImage(at index: Int) {
@@ -42,4 +47,39 @@ class PhotoDetailViewController: UIViewController {
         }.resume()
     }
     
+    private func setupScrollView() {
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 2.5
+    }
+    
+    private func setupTapGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        scrollView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc private func handleTap(_ sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: imageView)
+        
+        let currentScale = scrollView.zoomScale
+        let newScale = currentScale == 1.0 ? scrollView.maximumZoomScale : scrollView.minimumZoomScale
+        
+        let zoomRect = zoomRectForScale(scale: newScale, center: tapLocation)
+        scrollView.zoom(to: zoomRect, animated: true)
+    }
+
+    private func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+        let size = CGSize(width: scrollView.bounds.size.width / scale,
+                          height: scrollView.bounds.size.height / scale)
+        let origin = CGPoint(x: center.x - (size.width / 2.0),
+                             y: center.y - (size.height / 2.0))
+        return CGRect(origin: origin, size: size)
+    }
+    
+}
+
+extension PhotoDetailViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
